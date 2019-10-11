4474b091991ad53bebbddaece80b94c7d6cf480e
Jacob Perron <jacob@openrobotics.org>
2019-10-11 15:13:19 -0700

    Install fugitive

--- install.sh
+++ install.sh
@@ -6,5 +6,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+# fugitive
+git clone https://github.com/tpope/vim-fugitive.git -b v3.1 ~/.vim/bundle/vim-fugitive
+
 # Link .vimrc
 ln -fsv ${SCRIPT_DIR}/.vimrc $HOME/.vimrc
